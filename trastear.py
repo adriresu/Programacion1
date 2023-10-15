df593634cc11a2746347cce4c3e623fb35321133
adriresu <resuavidal@gmail.com>
2023-10-15 18:21:09 +0100

    FIX: Change map project workflow
    Change map  project from notebooks to individual python, change dependences used

--- trastear.py
+++ trastear.py
@@ -2,9 +2,9 @@ from tkinter import filedialog
 from PIL import Image
 import exifread
 import folium
-import tkinter as tk
-from tkinterhtml import HtmlFrame
 import os
+from tkinter import *
+import tkintermapview
 
 def convert_to_decimal_degrees(coord):
     if isinstance(coord.values[0], exifread.utils.Ratio):
@@ -20,52 +20,64 @@ def convert_to_decimal_degrees(coord):
     else:
         return None
 
-files_added = False
-root = tk.Tk()
-show_map_frame = None
-
-def show_map():
-    image_path = r'C:\Users\adrir\Desktop\alex.jpg'
+def get_coordinates(image_path):
     with open(image_path, 'rb') as image_file:
         metadata = exifread.process_file(image_file)
-
     # location data
-    # region
     latitude = convert_to_decimal_degrees(metadata.get('GPS GPSLatitude'))
     longitude = convert_to_decimal_degrees(metadata.get('GPS GPSLongitude'))
     latitude_ref = metadata.get('GPS GPSLatitudeRef')
     longitude_ref = metadata.get('GPS GPSLongitudeRef')
-    #endregion
+    return latitude, longitude
 
-    map = folium.Map(location=[latitude, longitude], zoom_start=2)
-    map.save("map.html")
-    return map
+# Add files
+root = Tk()
 
-def add_files():
-    global files_added
-    global show_map_frame
-    files = filedialog.askopenfilenames()
-    for file in files:
-        files_added = True
-        print("Archivo añadido:", file)
-    if files_added:
-        show_map()
-        show_map_frame = tk.Frame(root)
-        frame = HtmlFrame(root, horizontal_scrollbar="auto")
-        frame.set_content(os.getcwd() + "\\map.html")
-        frame.pack()
-    switch_view(show_map_frame)
+root.attributes('-fullscreen', True)
 
-def switch_view(frame):
-    add_files_frame.pack_forget()
-    if frame is not None:
-        frame.pack_forget()
-    frame.pack()
+root.title("AÑADIR ARCHIVOS")
 
-add_files_frame = tk.Frame(root)
-add_files_button = tk.Button(add_files_frame, text="Añadir archivos", command=add_files)
-add_files_button.pack()
+root.geometry("1820x980")
 
-switch_view(add_files_frame)
+my_label = LabelFrame(root)
+
+my_label.pack(pady=20)
+
+files = filedialog.askopenfilenames()
+
+for file in files:
+    get_coordinates(file)
+
+map_widget = tkintermapview.TkinterMapView(my_label, width=1920, height=1080, corner_radius=0)
+
+map_widget.pack()
+
+
+# def add_files():
+#     global files_added
+#     global show_map_frame
+#     files = filedialog.askopenfilenames()
+#     for file in files:
+#         files_added = True
+#         print("Archivo añadido:", file)
+#     if files_added:
+#         show_map()
+#         show_map_frame = tk.Frame(root)
+#         frame = HtmlFrame(root, horizontal_scrollbar="auto")
+#         frame.Loadfile(os.getcwd() + "\\map.html")
+#         frame.pack()
+#     switch_view(show_map_frame)
+
+# def switch_view(frame):
+#     add_files_frame.pack_forget()
+#     if frame is not None:
+#         frame.pack_forget()
+#     frame.pack()
+
+# add_files_frame = tk.Frame(root)
+# add_files_button = tk.Button(add_files_frame, text="Añadir archivos", command=add_files)
+# add_files_button.pack()
+
+# switch_view(add_files_frame)
 
 root.mainloop()
